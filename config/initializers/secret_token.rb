744407805f3f6d624e34d49f273ae7fde6b9b316
nai2 <nai2@nai2s-MacBook.local>
2013-08-11 22:51:34 +0930

    first go

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dwarfmongoose::Application.config.secret_token = '1dfc837ebe963ca5fca09fe6afb904de882251631358506be58ac5ca5a415f8d09f0fd55cd65a30ba9f86ac3c246accc15d6d6158da5e71c09165220382d7d03'
