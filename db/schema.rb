b9dc8274317fcea7cea279c548b4c67c17cce1b0
Jon S <jon@jojati.com>
2013-08-20 22:50:06 +0930

    ui

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(:version => 20130820084302) do
   create_table "contacts", :force => true do |t|
     t.string   "email"
     t.string   "name"
-    t.string   "message"
+    t.text     "message"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.string   "classification"
