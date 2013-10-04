a464e8abfd49ea951223e4709f232f2bd5e03547
Jon S <jon@jojati.com>
2013-10-04 20:29:34 +0930

    fix email

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -4,21 +4,23 @@ class FrontController < ApplicationController
   end
 
   def contact
-  	contact = Contact.new :email=>params[:email], :name=>params[:name], :message=>params[:message], 
-  							:classification=>params[:classification], :url=>params[:url], :fivethings=>params[:fivethings]
+    contact = Contact.new :email=>params[:email], :name=>params[:name], :message=>params[:message], 
+      :classification=>params[:classification], :url=>params[:url], :fivethings=>params[:fivethings]
 
-  	contact.save
+    contact.save
 
-    params[:upload].each do |file|
-    	attachment = Attachment.new
-    	attachment.upload = file
-    	attachment.save
-    	contact.attachments << attachment
-    	contact.save
+    if params[:upload].present?
+      params[:upload].each do |file|
+        attachment = Attachment.new
+        attachment.upload = file
+        attachment.save
+        contact.attachments << attachment
+        contact.save
+      end
     end
-   
 
-  	redirect_to root_url, notice: "Thanks for contacting us. We will get back to you ASAP."
+
+    redirect_to root_url, notice: "Thanks for contacting us. We will get back to you ASAP."
   end
 
 end
