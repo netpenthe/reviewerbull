af24aadd1f7d197bf7e98ffd6fdb8f189395446f
nai2 <adnaismi@yahoo.com>
2013-10-05 13:36:23 +0930

    replt to

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ActionMailer::Base
 
   def contact_email(contact)
   	@contact = contact
-    mail(to: "info@inputfarm.com", subject: 'Contact Form', from:@contact.email)
+    mail(to: "info@inputfarm.com", subject: 'Contact Form', from:"info@inputfarm.com", reply_to:@contact.email)
   end
 
 end
