6106f371b68e653f078ff51bee69b0ee63180986
nai2 <adnaismi@yahoo.com>
2013-10-11 22:29:47 +1030

    active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Dwarfmongoose::Application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
+  ActiveAdmin.routes(self)
 
    match 'front/contact' => 'front#contact'
 
