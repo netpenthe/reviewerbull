bf3f2f65eec50940fb7f82bdd00465ad45f4afbd
nai2 <adnaismi@yahoo.com>
2013-10-12 20:58:58 +1030

    tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131012102104) do
+ActiveRecord::Schema.define(:version => 20131012102606) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -64,8 +64,6 @@ ActiveRecord::Schema.define(:version => 20131012102104) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.string   "classification"
-    t.string   "url"
-    t.string   "fivethings"
     t.boolean  "sent"
   end
 
