8fec0cd826bdee269bfa9033cb55c708e96fea3b
Jon S <jon@jojati.com>
2013-10-14 21:18:25 +1030

    sample report route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Dwarfmongoose::Application.routes.draw do
 
+  match 'sample_report/:id' => 'front#sample_report'
   match 'front/contact' => 'front#contact'
   match 'front/submitted' => 'front#submitted', :as=>:front_submitted
 
