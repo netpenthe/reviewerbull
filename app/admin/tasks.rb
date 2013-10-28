b580e4635608ef3a5e0aa007d98ec5bd5575ffc8
nai2 <adnaismi@yahoo.com>
2013-10-28 21:48:52 +1030

    task view

--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -2,7 +2,9 @@ ActiveAdmin.register Task do
   
   index do
   	column :id
-  	column :user
+  	column :user do |task|
+         link_to task.user.email, admin_user_path(task.user.id) unless task.user.blank?
+        end
   	column :expert_name
   	column :url do |task|
   	  link_to task.url, task.url 
