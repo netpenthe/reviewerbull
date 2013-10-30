5049143886fbe1c0ed12dda6fbead32369fc38c5
nai2 <adnaismi@yahoo.com>
2013-10-30 21:50:32 +1030

    organise contacts page admin

--- app/admin/contacts.rb
+++ app/admin/contacts.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Contact do
-  
+  config.per_page = 10
+
+  index do    
+    selectable_column            
+    column :id            
+    column :email
+    column :name                     
+    column :message
+    column :classification  
+    column :created_at  
+    default_actions                   
+  end    
 end
