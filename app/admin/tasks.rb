6c16aa433c89a5eb6fa45593890f350c605153cf
nai2 <adnaismi@yahoo.com>
2013-10-31 20:32:42 +1030

    show tick/cross for paid

--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register Task do
   
   index do
+    selectable_column
   	column :id
   	column :user do |task|
          link_to task.user.email, admin_user_path(task.user.id) unless task.user.blank?
@@ -9,7 +10,7 @@ ActiveAdmin.register Task do
   	column :url do |task|
   	  link_to task.url, task.url 
   	end
-  	column :paid
+  	bool_column :paid
   	actions do |task|
     end
   end
