c55441745a459dae1fdd2163a3cd224703ae7e97
nai2 <adnaismi@yahoo.com>
2013-10-31 20:43:09 +1030

    show image

--- app/admin/attachments.rb
+++ app/admin/attachments.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register Attachment do
   
+	show do |user|
+      attributes_table do
+        row :id
+        row :uploadable_type
+        row :upload_file_name
+        row :upload_file_size
+        row :upload_content_type
+        row :created_at
+        row "Attachment" do |attachment|
+             image_tag attachment.upload.url(:medium)
+        end
+
+      end
+
+   end
+
+
 end
