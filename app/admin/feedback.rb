d98b95a648dafd55afa32b931811770185b0d2d7
nai2 <adnaismi@yahoo.com>
2013-11-12 23:34:20 +1030

    show task for the feedback

--- app/admin/feedback.rb
+++ app/admin/feedback.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register Feedback do     
 
+
+  index do
+        column :id
+        column :created_at
+        column :comment
+        column :score
+
+        column "Task" do |fb|
+          link_to "Task", admin_task_path(fb.task) unless fb.task.blank?
+        end
+
+        actions :defaults=>false do |fb|
+          link_to "Edit", edit_admin_feedback_path(fb)
+        end
+  end
+
+
+
 end                                   
